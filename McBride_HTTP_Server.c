c65fb9bb9d9b93ee0d9ef652dcb457cbe50bcddc
Justin McBride <justin@dare599z.com>
2015-09-06 02:50:11 -0600

    first

---
+++ McBride_HTTP_Server.c
@@ -0,0 +1,206 @@
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <sys/errno.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define QLEN      32  /* maximum connection queue length  */
+#define BUFSIZE   4096
+
+extern int  errno;
+int   errexit(const char *format, ...);
+int   passivesock(const char *portnum, int qlen);
+int   parseRequest(int fd);
+
+/*------------------------------------------------------------------------
+ * main - Concurrent TCP server for ECHO service
+ *------------------------------------------------------------------------
+ */
+int
+main(int argc, char *argv[])
+{
+  char  *portnum = "5004";  /* Standard server port number  */
+  struct sockaddr_in fsin;  /* the from address of a client */
+  int msock;      /* master server socket   */
+  fd_set  rfds;     /* read file descriptor set */
+  fd_set  afds;     /* active file descriptor set */
+  unsigned int  alen;   /* from-address length    */
+  int fd, nfds;
+  
+  switch (argc) {
+  case  1:
+    break;
+  case  2:
+    portnum = argv[1];
+    break;
+  default:
+    errexit("usage: %s [port]\n",argv[0]);
+  }
+
+  msock = passivesock(portnum, QLEN);
+
+  nfds = msock+1;
+  FD_ZERO(&afds);
+  FD_SET(msock, &afds);
+
+  while (1) {
+    memcpy(&rfds, &afds, sizeof(rfds));
+
+    if (select(nfds, &rfds, (fd_set *)0, (fd_set *)0,
+        (struct timeval *)0) < 0)
+      errexit("select: %s\n", strerror(errno));
+    if (FD_ISSET(msock, &rfds)) {
+      int ssock;
+
+      alen = sizeof(fsin);
+      ssock = accept(msock, (struct sockaddr *)&fsin,
+        &alen);
+      if (ssock < 0)
+        errexit("accept: %s\n",
+          strerror(errno));
+      FD_SET(ssock, &afds);
+    }
+    for (fd=0; fd<nfds; ++fd)
+      if (fd != msock && FD_ISSET(fd, &rfds))
+        if (parseRequest(fd) == 0) {
+          (void) close(fd);
+          FD_CLR(fd, &afds);
+        }
+  }
+}
+
+/*------------------------------------------------------------------------
+ * parseRequest - look at the request sent by the client and 
+ * respond appropiately
+ *------------------------------------------------------------------------
+ */
+int
+parseRequest(int fd)
+{
+  char  buf[BUFSIZ];
+  int cc;
+
+  char method[15];
+  char uri[256];
+  char version[256];
+
+  const char* METHOD_GET = "GET";
+  const char* test_send = "HTTP/1.1 200 OK\r\n"
+                          "Date: Tue, 16 Feb 2010 19:21:24 GMT\r\n"
+                          "Content-Type: text/html;\r\n\r\n"
+                          "<html><head><title>First Web Server!</title>\r\n";
+
+  cc = read(fd, buf, sizeof buf);
+  if (cc < 0) errexit("parseRequest read: %s\n", strerror(errno));
+
+  if (cc) {
+    // have data
+
+    int read = sscanf(buf, "%s %s %s", method, uri, version);
+
+    if ( read != 3 ) {
+      char* bad = "bad";
+      write(fd, bad, strlen(bad));
+      printf("bad args\n");
+    }
+    else {
+      if ( strcmp( METHOD_GET, method) == 0) {
+        printf("----------------------------------------------\n");
+        printf("REQUEST FROM CLIENT : \n%s\n", buf);
+        printf("----------------------------------------------\n");
+        printf("SERVER RESPONSE:\n%s\n", test_send);
+        printf("----------------------------------------------\n");
+        write(fd, test_send, strlen(test_send));
+        close(fd);
+        return sizeof test_send;
+      }
+      else {
+        printf( "got METHOD: [%s]\n", method );
+        // printf( "buf = %s\n", buf );
+      }
+
+    }
+  }
+
+  else {
+    // nothing useful to do
+  }
+  return cc;
+}
+
+/*------------------------------------------------------------------------
+ * errexit - print an error message and exit
+ *------------------------------------------------------------------------
+ */
+int
+errexit(const char *format, ...)
+{
+  va_list args;
+
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  exit(1);
+}
+
+/*------------------------------------------------------------------------
+ * passivesock - allocate & bind a server socket using TCP
+ *------------------------------------------------------------------------
+ */
+int
+passivesock(const char *portnum, int qlen)
+/*
+ * Arguments:
+ *      portnum   - port number of the server
+ *      qlen      - maximum server request queue length
+ */
+{
+  struct sockaddr_in sin; /* an Internet endpoint address  */
+  int     s;              /* socket descriptor             */
+
+  memset(&sin, 0, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = INADDR_ANY;
+
+/* Map port number (char string) to port number (int) */
+  if ((sin.sin_port=htons((unsigned short)atoi(portnum))) == 0)
+          errexit("can't get \"%s\" port number\n", portnum);
+
+/* Allocate a socket */
+  s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (s < 0)
+      errexit("can't create socket: %s\n", strerror(errno));
+
+/* Bind the socket */
+  if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+      fprintf(stderr, "can't bind to %s port: %s; Trying other port\n",
+          portnum, strerror(errno));
+      sin.sin_port=htons(0); /* request a port number to be allocated
+                             by bind */
+      if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+          errexit("can't bind: %s\n", strerror(errno));
+      else {
+          int socklen = sizeof(sin);
+
+          if (getsockname(s, (struct sockaddr *)&sin, &socklen) < 0)
+                  errexit("getsockname: %s\n", strerror(errno));
+          printf("New server port number is %d\n", ntohs(sin.sin_port));
+      }
+  }
+
+  if (listen(s, qlen) < 0)
+      errexit("can't listen on %s port: %s\n", portnum, strerror(errno));
+  return s;
+}
+
---
+++ McBride_Server.cpp
@@ -0,0 +1,475 @@
+#include <arpa/inet.h>
+
+#include <event2/listener.h>
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+
+inline bool file_exists (const std::string& name) {
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
+}
+
+std::string MakeSuccessHeader(
+  const std::string& mime_type,
+  const size_t length,
+  const std::map<std::string, std::string> other_attrs
+  )
+{
+  std::ostringstream ss;
+  ss << 
+        "HTTP/1.1 200 OK\r\n" <<
+        "Content-Type: " << mime_type << "\r\n" <<
+        "Content-Length: " << length << "\r\n"
+  ;
+
+  ss << "\r\n"; // have this last to separate the header from content
+  for (std::map<std::string, std::string>::const_iterator it = other_attrs.begin(); it != other_attrs.end(); ++it)
+  {
+    ss << it->first << ": " << it->second << "\r\n";
+  }
+  return ss.str();
+}
+
+class ServerConf {
+public:
+  ServerConf() :
+    m_indices()
+  {
+
+  }
+  bool ParseConfFile(const std::string& filename)
+  {
+    std::ifstream file;
+    file.open(filename);
+    if ( file.is_open() )
+    {
+      std::string line;
+      // std::cout << "Config file:" << std::endl;
+      while ( getline(file, line) )
+      {
+
+        if (line.at(0) == '#') continue;
+        // std::cout << line << std::endl;
+        std::istringstream ss(line);
+
+        std::string first;
+        if ( !( ss >> first ) ) {
+          std::cerr << "  \t***Error reading line..." << std::endl;
+        }
+
+        if ( first.compare("Listen") == 0 )
+        {
+          if ( !(ss >> m_port) ) {
+            std::cerr << "\tError.. Need Listen <int>" << std::endl;
+            return false;
+          }
+          std::cout << "\tUsing port " << m_port << std::endl;
+        }
+        else if ( first.compare("DocumentRoot") == 0 )
+        {
+          if ( !(ss >> m_root) ) {
+            std::cerr << "\tError.. Need DocumentRoot <string>" << std::endl;
+            return false;
+          }
+          // sanitize the dir of quotes
+          m_root.erase(std::remove(m_root.begin(), m_root.end(), '\"'), m_root.end());
+          std::cout << "\tUsing root " << m_root << std::endl;
+        }
+        else if ( first.compare("DirectoryIndex") == 0 )
+        {
+          std::string index;
+          if ( !(ss >> index) ) {
+            std::cerr << "\tError.. Need DirectoryIndex <string> [<string ...]" << std::endl;
+            return false;
+          }
+          m_indices.push_back(index);
+          while ( ss >> index ) {
+            m_indices.push_back(index);
+          }
+
+          std::cout << "\tUsing directory indices:" << std::endl;
+          for (std::vector<std::string>::iterator it = m_indices.begin(); it != m_indices.end(); ++it)
+          {
+            std::cout << "\t\t" << *it << std::endl;
+          }
+          // std::cout << "\tUsing root " << m_root << std::endl;
+        }
+        else if ( first.at(0) == '.' )
+        {
+          std::string ct;
+          if ( !(ss>> ct) )
+          {
+            std::cerr << "\tError.. Need \".extension Content-Type\"" << std::endl;
+            continue;
+          }
+          m_file_types.push_back( file_type(first, ct) );
+        }
+      }
+
+      for (std::vector<file_type>::iterator it = m_file_types.begin(); it != m_file_types.end(); ++it)
+      {
+        std::cout << "Allowing file type " << it->extension << "(" << it->content_type << ")" << std::endl;
+      }
+    }
+    else
+    {
+      std::cerr << "Error opening file.. : " << filename << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  struct file_type {
+    file_type(std::string ext, std::string ct) {
+      extension = ext;
+      content_type = ct;
+    }
+    std::string extension;
+    std::string content_type;
+  };
+
+  int port() const {
+    return m_port;
+  }
+
+  std::vector<std::string> indicies() const {
+    return m_indices;
+  }
+
+  std::string file_root() const {
+    return m_root;
+  }
+
+private:
+  int m_port;
+  std::string m_root;
+  std::vector<std::string> m_indices;
+  std::vector<file_type> m_file_types;
+} sc;
+
+class http_request {
+public:
+  http_request(std::string serverRoot) {
+    m_root = serverRoot;
+  }
+
+  void uri_set(const std::string& uri)
+  {
+    m_uri = uri;
+    m_full_uri = m_root + m_uri;
+  }
+
+  const std::string& uri() const {
+    return m_uri;
+  }
+
+  const std::string& full_uri() const {
+    return m_full_uri;
+  }
+
+  std::string method;
+  std::string http_version;
+  std::map<std::string, std::string> other_attrs;
+private:
+  std::string m_root;
+  std::string m_uri;
+  std::string m_full_uri;
+};
+
+const static std::string e500 = "HTTP/1.1 500 Internal Server Error: cannot allocate memory\n";
+const static std::string e400 = "HTTP/1.1 400 Bad Request: ";
+const static std::string e404 = "HTTP/1.1 404 Not Found: ";
+
+void callback_data_written(bufferevent *bev, void *ctx)
+{
+  std::cout << "Done writing... Closing..." << std::endl;
+  bufferevent_free(bev);
+}
+
+static void callback_event(bufferevent *event, short events, void *context)
+{
+  if ( events & BEV_EVENT_ERROR )
+  {
+    std::cout << "Error in the bufferevent" << std::endl;
+    // error frmo bufferevent
+  }
+  if ( events & ( BEV_EVENT_EOF | BEV_EVENT_ERROR ) )
+  {
+    std::cout << "EOF in bufferevent.. closing" << std::endl;
+    bufferevent_free(event);
+  }
+}
+
+void callback_read(bufferevent *ev, void *context)
+{
+  std::string error_string;
+  http_request req(sc.file_root());
+  evbuffer *input = bufferevent_get_input(ev);
+  evbuffer *output = bufferevent_get_output(ev);
+
+  size_t n;
+  int result = 0;
+  const char* METHOD_GET = "GET";
+  const char* URI_ROOT = "/";
+
+  /*
+    Enter a loop to construct the request
+    and we will process a (valid) request
+    after this loop
+  */
+  for (int i = 1; ; ++i)
+  {
+    bufferevent_lock(ev);
+    char *line = evbuffer_readln(input, &n, EVBUFFER_EOL_CRLF);
+    bufferevent_unlock(ev);
+    if ( !line ) {
+      if ( i == 1 ) {
+        std::cout << "Opened connection but no input... CLOSING.." << std::endl;
+        bufferevent_free(ev);
+        return;
+      }
+      break;
+    }
+    if ( i == 1 ) {
+      // First line of HTTP Request...
+      // Should have METHOD URI HTTP_VERSION
+      std::istringstream ss(line);
+
+      // Parse out the method
+      if ( !(ss>>req.method) ) {
+        std::cerr << "Unable to parse HTTP Method." << std::endl
+                  << "\t" << line << std::endl;
+        goto request_error;
+      }
+      std::cout << "parsed method = " << req.method << std::endl;
+
+      // Parse out the uri
+      std::string uri;
+      if ( !(ss>>uri) ) {
+        std::cerr << "Unable to parse HTTP URI." << std::endl
+                  << "\t" << line << std::endl;
+        goto request_error;
+      }
+      req.uri_set(uri);
+      std::cout << "parsed uri = " << req.uri() << std::endl;
+      std::cout << "parsed full_uri = " << req.full_uri() << std::endl;
+
+      // Parse out the http version
+      if ( !(ss>>req.http_version) ) {
+        std::cerr << "Unable to parse HTTP Version." << std::endl
+                  << "\t" << line << std::endl;
+        goto request_error;
+      }
+      std::cout << "parsed ver = " << req.http_version << std::endl;
+
+      if ( req.method.compare(METHOD_GET) == 0 ) {
+        // try and open the file requested
+        if ( req.uri().compare(URI_ROOT) == 0 )
+        {
+          std::cout << "Client requested root.." << std::endl;
+          std::vector<std::string> indicies = sc.indicies();
+          bool rootFound = false;
+          for (std::vector<std::string>::iterator it = indicies.begin(); it != indicies.end(); ++it)
+          {
+            std::string f(sc.file_root());
+            f += *it;
+            std::cout << "Checking if root file exists... " << f << std::endl;
+            if ( file_exists(f) ) {
+              std::cout << "\ttrue" << std::endl;
+              req.uri_set(*it);
+              rootFound = true;
+              break;
+            }
+          }
+          if ( !rootFound )
+          {
+            error_string = req.uri();
+            goto no_file;
+          }
+        }
+        else
+        {
+          std::cout << "Checking if file exists... " << req.full_uri();
+          bool f = file_exists(req.full_uri());
+          std::cout << ( f ? "\t[true]" : "\t[false]" ) << std::endl;
+          if ( !f ) {
+            error_string = req.uri();
+            goto no_file;
+          }
+          
+        }
+      }
+      else {
+        std::cout << "Client requested method " << req.method << std::endl;
+        error_string = "Invalid Method: " + req.method;
+        goto bad_method;
+      }
+    }
+    else {
+      /*
+      This is any other content sent along with the request,
+      such as Connection: keep-alive
+      */
+    }
+  }
+
+  /*
+    Now we need to actually go through and process
+    the request..
+  */
+
+  if (req.method.compare(METHOD_GET) == 0)
+  {
+    int fd = open( req.full_uri().c_str(), O_RDONLY );
+    if ( fd < 0 ) {
+      std::cerr << "<<ERROR>> Couldn't open file " << req.full_uri() << std::endl;
+      return;
+    }
+    struct stat fd_stat;
+    fstat(fd, &fd_stat); // get the file size that we're sending to the buffer
+
+    std::string header = MakeSuccessHeader("text/html", fd_stat.st_size, req.other_attrs);
+
+    std::cout << "Sending file...";
+    evbuffer_lock(output);
+    evbuffer_add(output, header.c_str(), header.length() );
+    result = evbuffer_add_file(output, fd, 0, fd_stat.st_size);
+    evbuffer_unlock(output);
+    std::cout << "done, result = " << result << std::endl;
+    bufferevent_setcb(ev, callback_read, callback_data_written, callback_event, NULL);
+
+    return;
+  } // GET method
+  else
+  {
+    /*
+      at this point, we only implement the GET method
+      and the code should never get here, but who knows..
+      crazier stuff has happened
+    */
+
+    std::cerr << "<<ERROR>> Something went wrong... shouldn't have accepted this non-GET parameter.." << std::endl;
+    bufferevent_free(ev); // just close the connection
+  } // non-GET method
+
+no_file:
+  {
+    std::string es;
+    es += e404;
+    es += error_string;
+    es += "\n";
+    bufferevent_lock(ev);
+    result = bufferevent_write( ev, es.c_str(), es.length() );
+    bufferevent_unlock(ev);
+    bufferevent_setcb(ev, callback_read, callback_data_written, callback_event, NULL);
+    return;
+  }
+  
+request_error:
+  bufferevent_lock(ev);
+  result = bufferevent_write( ev, e500.c_str(), e500.length() );
+  bufferevent_unlock(ev);
+  bufferevent_setcb(ev, callback_read, callback_data_written, callback_event, NULL);
+  return;
+
+bad_method:
+  {
+    std::string es;
+    es += e400;
+    es += error_string;
+    es += "\n";
+    bufferevent_lock(ev);
+    result = bufferevent_write( ev, es.c_str(), es.length() );
+    bufferevent_unlock(ev);
+    bufferevent_setcb(ev, callback_read, callback_data_written, callback_event, NULL);
+    return;
+  }
+}
+
+
+
+static void callback_accept_connection(
+  evconnlistener *listener,
+  evutil_socket_t fd,
+  sockaddr *address,
+  int socklen,
+  void *context
+  )
+{
+  event_base *base = evconnlistener_get_base(listener);
+  bufferevent *bev = bufferevent_socket_new(base,
+                                            fd,
+                                            BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
+  printf("***Connection acccepted. Port %d***\n", (int)fd);
+  bufferevent_setcb(bev, callback_read, NULL, callback_event, NULL);
+  bufferevent_setwatermark(bev, EV_WRITE, 0, 0);
+  bufferevent_enable(bev, EV_READ|EV_WRITE);
+}
+
+static void
+callback_accept_error(struct evconnlistener *listener, void *ctx)
+{
+  struct event_base *base = evconnlistener_get_base(listener);
+  int err = EVUTIL_SOCKET_ERROR();
+  fprintf(stderr, "Got an error %d (%s) on the listener. "
+          "Shutting down.\n", err, evutil_socket_error_to_string(err));
+
+  event_base_loopexit(base, NULL);
+}
+
+int main(int argc, char** argv)
+{
+  // ServerConf sc;
+  if ( !sc.ParseConfFile("./ws.conf") ) {
+    std::cerr << "Errors while parsing the configuration file... Exiting" << std::endl;
+    return -1;
+  }
+
+  event_base *base = event_base_new();
+  if ( !base )
+  {
+    std::cerr << "Error creating an event loop.. Exiting" << std::endl;
+    return -2;
+  }
+
+  sockaddr_in sin; 
+  memset(&sin, 0, sizeof sin);
+
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = 0; // local host
+  sin.sin_port = htons(sc.port());
+
+  evconnlistener *listener = evconnlistener_new_bind(
+                                     base,
+                                     callback_accept_connection,
+                                     NULL,
+                                     LEV_OPT_CLOSE_ON_FREE|LEV_OPT_REUSEABLE,
+                                     -1,
+                                     (sockaddr*)&sin,
+                                     sizeof sin
+                                     );
+
+  if ( !listener )
+  {
+    std::cerr << "Error creating a TCP socket listener.. Exiting" << std::endl;
+    return -3;
+  }
+
+  evconnlistener_set_error_cb(listener, callback_accept_error);
+  event_base_dispatch(base);
+}
+
